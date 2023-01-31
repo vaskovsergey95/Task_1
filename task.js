0508189851eac308fac2a299fc4cd41ee06f4225
Сергей <vaskovsergey95@gmail.com>
2023-02-01 00:28:37 +0500

    first commit

---
+++ task.js
@@ -0,0 +1,20 @@
+"use strict";
+let d;
+let finish;
+function solveEquation(a, b ,c) {
+    d = b ** 2 - 4 * a * c;
+    if (d < 0) {
+        return finish = [];
+    } else if (d === 0) {
+        finish = [Math.sqrt(d)];
+        return finish;
+    } else if (d > 0) {
+        let x1 = (-b + Math.sqrt(d)) / (2 * a);
+        let x2 = (-b - Math.sqrt(d)) / (2 * a);
+        return finish = [x1, x2];
+    }
+}
+solveEquation(3, -4, -20);
+console.log(finish)
+
+
