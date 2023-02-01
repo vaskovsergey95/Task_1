e92bfb76c6f53969fd062971317e94e7ad4091e4
Сергей <vaskovsergey95@gmail.com>
2023-02-01 07:47:18 +0500

    4 commit

--- task.js
+++ task.js
@@ -1,20 +1,21 @@
 "use strict";
-let d;
-let finish;
+
+let decision;
 function solveEquation(a, b ,c) {
+    let d;
     d = b ** 2 - 4 * a * c;
     if (d < 0) {
-        return finish = [];
+        return decision = [];
     } else if (d === 0) {
-        finish = [Math.sqrt(d)];
-        return finish;
+        decision = [Math.sqrt(d)];
+        return decision;
     } else if (d > 0) {
         let x1 = (-b + Math.sqrt(d)) / (2 * a);
         let x2 = (-b - Math.sqrt(d)) / (2 * a);
-        return finish = [x1, x2];
+        return decision = [x1, x2];
     }
 }
 solveEquation(3, -6, -110);
-console.log(finish)
+console.log(decision)
 
 
